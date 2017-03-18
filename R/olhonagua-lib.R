a129ea439648a8bc507fa4bc82b49ebf378ca410
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2017-03-18 18:58:43 -0300

    Calcula consumo mês a mês. Gota a gota.

--- R/olhonagua-lib.R
+++ R/olhonagua-lib.R
@@ -46,3 +46,56 @@ get_repositorios <- function(ids){
     }
     return(repositorios)
 }
+
+limpa_e_add_sofrendo_delta <- function(historicos){
+    historicos %>% 
+        filter(Ano >= 2012, complete.cases(historicos)) %>% 
+        group_by(id) %>% 
+        arrange(Data) %>% 
+        mutate(sofrendo = !any(Ano >= 2015 & VolumePercentual > 60), 
+               Volume = as.numeric(Volume),
+               delta = c(0, diff(Volume))) %>% 
+        return()
+}
+
+filtra_decidas = function(historicos){
+    d = historicos %>% 
+        group_by(id) %>% 
+        arrange(Data) %>% 
+        filter(delta < 0 | 
+                   lead(delta) < 0 | 
+                   (lead(delta) == 0 & delta < 0) | 
+                   (delta == 0 & lag(delta) <0)) 
+    
+     d %>% 
+        group_by(id) %>% 
+        arrange(Data) %>% 
+        mutate(inicio = (Data == first(Data)) | 
+                   delta > 0 |  
+                   as.period(Data - lag(Data)) > days(60), 
+               fim = (Data == last(Data)) | lead(delta) > 0)  %>% 
+        ungroup() %>% 
+        arrange(id, Data) %>% 
+        mutate(sequencia = cumsum(inicio)) %>% 
+        filter(abs(delta) < 100) %>% 
+        return()
+}
+
+cria_seqs_inicio_fim <- function(decidas){
+    decidas %>% group_by(sequencia) %>% 
+        #filter(Data == first(Data) | Data == last(Data)) %>% 
+        filter(n() >1) %>% 
+        return()
+}
+
+get_consumo_recente <- function(reservatorio, sumario){
+    library(zoo)
+    r = sumario %>% 
+        filter(id == reservatorio) %>% 
+        arrange(Data) %>% 
+        group_by(sequencia) %>% 
+        mutate(delta = c(0, diff(Volume)), 
+               dias = c(0, diff(Data)),
+               consumo = ifelse(dias == 0, 0, abs(delta)/dias),
+               consumo.r = rollmean(x = consumo, 6, na.pad = TRUE, fill = NA))
+}
