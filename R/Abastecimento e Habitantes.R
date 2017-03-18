e9dda1ced52e605534f005e1a9c8f728a5245260
CelioBarros <celio.barros.filho@gmail.com>
2017-03-18 17:35:06 -0300

    Script que determina a populacao das cidades cujo reservatorios são conhecidos

---
+++ R/Abastecimento e Habitantes.R
@@ -0,0 +1,19 @@
+populacao <- function(){
+  library(dplyr, warn.conflicts = FALSE)
+  abastecimento = read.csv('../olhonagua/abastecimento_municipio.csv', fileEncoding = "utf-8")
+  info_gerais = read.csv('../snis/informacoes_gerais.csv', fileEncoding = "utf-8")
+  
+  info_gerais_ultimas = filter(info_gerais, Ano.de.Referência == max(Ano.de.Referência))
+  abastecimento$GEOCODIGO1 <- as.numeric(substr(abastecimento$GEOCODIGO,0,6))
+  comparacao = right_join(info_gerais_ultimas, abastecimento, by = c("Código.do.Município" = "GEOCODIGO1"))
+  k = rename(comparacao,
+     Geocodigo = Código.do.Município,
+     Municipio = Município,
+     Estado = Estado,
+     Pop_total = POP_TOT...População.total.do.município..Fonte..IBGE....Habitantes.,
+     Pop_urbana_total = POP_URB...População.urbana.do.município..Fonte..IBGE...Habitantes.
+  )
+  
+  result = distinct(k, Geocodigo, Municipio, Estado, Pop_total, Pop_urbana_total)
+  return (result)
+}
